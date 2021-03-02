2da5bf6e5101dc0960c747bcc8f5c3d1a3e662d1
Polina Chernomaz <pachernomaz@gmail.com>
2021-03-01 23:25:26 -0500

    Create navbar

--- src/components/TheHeader.vue
+++ src/components/TheHeader.vue
@@ -1,3 +1,52 @@
 <template>
-  <h1>The Header Component</h1>
+  <b-navbar
+    toggleable="lg"
+    type="light"
+    variant="light"
+  >
+    <b-navbar-brand to="/">
+      Stock Trader
+    </b-navbar-brand>
+
+    <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+
+    <b-collapse
+      id="nav-collapse"
+      is-nav
+    >
+      <b-navbar-nav>
+        <b-nav-item to="/portfolio">
+          Portfolio
+        </b-nav-item>
+        <b-nav-item to="/stocks">
+          Stocks
+        </b-nav-item>
+      </b-navbar-nav>
+    </b-collapse>
+    <!-- Right aligned nav items -->
+    <b-navbar-nav class="ml-auto">
+      <b-nav-form>
+        <b-nav-item>End Day</b-nav-item>
+        <b-nav-item-dropdown
+          text="Save & Load"
+          right
+        >
+          <b-dropdown-item href="#">
+            Save Data
+          </b-dropdown-item>
+          <b-dropdown-item href="#">
+            Load Data
+          </b-dropdown-item>
+        </b-nav-item-dropdown>
+      </b-nav-form>
+    </b-navbar-nav>
+  </b-navbar>
 </template>
+
+<style lang="scss" scoped>
+@import '@/styles/main.scss';
+
+nav {
+  @include margin(bottom 35px);
+}
+</style>
