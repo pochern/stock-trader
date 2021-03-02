9f1e532fe99113c09c780d23cfb59a3c6adf4fba
Polina Chernomaz <pachernomaz@gmail.com>
2021-03-01 23:04:18 -0500

    Do not self-close components

--- .eslintrc.js
+++ .eslintrc.js
@@ -17,7 +17,8 @@ module.exports = {
     'vue/html-self-closing': ['error', {
       'html': {
         normal: 'never',
-        void: 'always'
+        void: 'never',
+        component: 'never'
       }
     }]
   },
