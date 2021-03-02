9cc112eee8848a9b79db5b5f7c0331e184846f4e
Polina Chernomaz <pachernomaz@gmail.com>
2021-03-01 23:24:28 -0500

    Add styling to app

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,35 @@
 <template>
   <div id="app">
-    <router-view></router-view>
+    <the-header></the-header>
+    <b-row>
+      <b-col col="12">
+        <router-view></router-view>
+      </b-col>
+    </b-row>
   </div>
 </template>
 
+<script lang="ts">
+import Vue from 'vue';
+import TheHeader from '@/components/TheHeader.vue';
+
+export default Vue.extend({
+  components: {
+    TheHeader,
+  },
+});
+</script>
+
 <style lang="scss">
+@import '@/styles/main.scss';
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
 }
 
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
+body {
+  @include padding(all (30px 60px));
 }
 </style>
