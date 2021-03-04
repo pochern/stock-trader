909922ca257ed4c4849bcb902b224a658ad5b3b8
Polina Chernomaz <pachernomaz@gmail.com>
2021-03-03 21:17:42 -0500

    Create Stock card

--- src/components/stocks/Stock.vue
+++ src/components/stocks/Stock.vue
@@ -1,3 +1,34 @@
 <template>
-  <h1>The Stocks - Stock Component</h1>
+  <b-col
+    sm="6"
+    md="4"
+  >
+    <b-card
+      header="Card Header"
+      header-text-variant="white"
+      header-tag="header"
+      header-bg-variant="success"
+      title="Title"
+    >
+      <template #header>
+        <h3>
+          NAME
+          <small>(Price: PRICE)</small>
+        </h3>
+      </template>
+      <b-card-text>Header and footers variants.</b-card-text>
+    </b-card>
+  </b-col>
 </template>
+
+<style lang="scss" scoped>
+@import '@/styles/main.scss';
+
+h3 {
+  @include margin(bottom 0);
+}
+
+.card {
+  @include margin(bottom 30px);
+}
+</style>
