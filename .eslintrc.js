754be8c5d3afe45bcedd077212a58d4fc1130ba5
Polina Chernomaz <pachernomaz@gmail.com>
2021-03-29 23:53:38 -0400

    Allow for local variable to share the same name as a variable in its containing scope

--- .eslintrc.js
+++ .eslintrc.js
@@ -14,6 +14,7 @@ module.exports = {
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'no-shadow': ['error', { 'allow': ['state'] }],
     'vue/html-self-closing': ['error', {
       html: {
         normal: 'never',
