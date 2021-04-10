7b02b77e7680ac67d1a1ef713c4dc53a09574f80
Polina Chernomaz <pachernomaz@gmail.com>
2021-04-10 00:56:29 -0400

    Sell stocks on Portfolio page

--- src/components/portfolio/Stock.vue
+++ src/components/portfolio/Stock.vue
@@ -1,3 +1,90 @@
 <template>
-  <h1>The Portfolio - Stock Component</h1>
+  <b-col
+    sm="6"
+    md="4"
+  >
+    <b-card
+      header="Card Header"
+      header-text-variant="white"
+      header-tag="header"
+      header-bg-variant="info"
+    >
+      <template #header>
+        <h3>
+          {{ stock.name }}
+          <small>(Price: {{ stock.price }} | Quantity: {{ stock.quantity }})</small>
+        </h3>
+      </template>
+      <b-card-text>
+        <input
+          v-model.number="quantity"
+          class="form-control"
+          type="number"
+          placeholder="Quantity"
+        >
+        <b-button
+          variant="success"
+          :disabled="quantity <= 0 || !Number.isInteger(quantity)"
+          @click="sellStock"
+        >
+          Sell
+        </b-button>
+      </b-card-text>
+    </b-card>
+  </b-col>
 </template>
+
+<script lang="ts">
+import Vue from 'vue';
+import { mapActions } from 'vuex';
+
+export default Vue.extend({
+  props: {
+    stock: {
+      type: Object,
+      required: true,
+    },
+  },
+  data() {
+    return {
+      quantity: 0,
+    };
+  },
+  methods: {
+    ...mapActions({
+      placeSellOrder: 'sellStock',
+    }),
+    sellStock() {
+      const order = {
+        stockId: this.stock.id,
+        stockPrice: this.stock.price,
+        quantity: this.quantity,
+      };
+      // Action
+      this.placeSellOrder(order);
+      this.quantity = 0;
+    },
+  },
+});
+</script>
+
+<style lang="scss" scoped>
+@import '@/styles/main.scss';
+
+h3 {
+  @include margin(bottom 0);
+}
+
+.form-control {
+  @include w-h(200px, undefined);
+}
+
+.card {
+  @include margin(bottom 30px);
+}
+
+.card-text {
+  display: flex;
+  justify-content: space-between;
+}
+</style>
--- src/pages/Portfolio.vue
+++ src/pages/Portfolio.vue
@@ -1,3 +1,26 @@
 <template>
-  <h1>This is the Portfolio</h1>
+  <div>
+    <app-stock
+      v-for="stock in stocks"
+      :key="stock.id"
+      :stock="stock"
+    />
+  </div>
 </template>
+
+<script lang="ts">
+import Vue from 'vue';
+import { mapGetters } from 'vuex';
+import Stock from '@/components/portfolio/Stock.vue';
+
+export default Vue.extend({
+  components: {
+    appStock: Stock,
+  },
+  computed: {
+    ...mapGetters({
+      stocks: 'stockPortfolio',
+    }),
+  },
+});
+</script>
